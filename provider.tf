7190dedb3ad180e3154a40536e98e2a951443c40
Carl <carl5184@yahoo.com>
2023-10-12 14:27:32 -0400

    add terra files

--- provider.tf
+++ provider.tf
@@ -1,1 +1,11 @@
-provider "aws" {}
+provider "aws" {
+  region = "us-east-1"
+}
+# provider "aws" {
+#   region = "us-east-1"
+# }
+
+# provider "aws" {
+#   alias  = "terra_user2"
+#   region = "ap-south-1"
+# }
